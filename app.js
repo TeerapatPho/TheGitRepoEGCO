cfbca253156230149886b725b07427e3ced8bb1e
Fankrbb <fankrbb@gmail.com>
2024-04-03 15:42:33 +0700

    feat(.gitignore): add .gitignore

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello World")
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World Here are the update version'))
+
+app.listen(3000, () => console.log('server start'))
